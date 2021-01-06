d34247e3d96215de7e6ad7d194c00dd80fad416c
KrishnaSingh18 <75606526+KrishnaSingh18@users.noreply.github.com>
2021-01-06 18:35:52 +0530

    Add files via upload

---
+++ log.js
@@ -0,0 +1,33 @@
+class log{
+    
+    constructor(x,y,height,angle){
+     
+        var options ={
+             restitution: 0.2,
+             friction:0.3,
+             density:1.0
+           
+        }
+        this.body = Bodies.rectangle(x,y,30,height,options);
+        this.width = 30;
+        this.height = height;
+        Matter.Body.setAngle(this.body,angle);
+
+        World.add(world,this.body)
+    }
+
+    display(){
+         var pos = this.body.position;
+         var angle = this.body.angle;
+       push();
+       translate(pos.x,pos.y); 
+       rotate(angle);
+        rectMode(CENTER);
+        strokeWeight(4);
+        stroke("blue");
+        fill("green");
+        rect(0,0,this.width,this.height);
+        pop();
+       
+    }
+};
